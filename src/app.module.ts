3ff1c9d282a8a7d5db293a8555cce2b7a7242923
Kasra Karami <Kasra.Karami.Work@gmail.com>
2023-01-06 18:36:54 +0330

    add typeorm to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppUserModule } from './app-user/app-user.module';
 
 @Module({
-  imports: [AppUserModule],
-  controllers: [],
-  providers: []
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    AppUserModule
+  ]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
