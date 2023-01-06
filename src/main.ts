fffd3f38c9820998960d11947e90f397de57504f
Kasra Karami <Kasra.Karami.Work@gmail.com>
2023-01-06 18:03:25 +0330

    init microservice-blog-user

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger(bootstrap.name);
+
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Other')
+    .setDescription('User service work on user and authentication')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app
+    .listen(port, '0.0.0.0')
+    .then(() => logger.log(`User service is running on port ${port}`))
+    .catch((error) => logger.error(error));
+}
+bootstrap();
