adb9ba490f533b5023a1732220efd3ee2cbb2386
Kasra Karami <Kasra_K2K@yahoo.com>
2023-01-09 21:03:55 +0330

    Add config files #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppUserModule } from './app-user/app-user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? ['.env.development', '.env']
+          : process.env.NODE_ENV === 'production'
+          ? ['.env.production', '.env']
+          : ['.env.development.local', '.env'],
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
